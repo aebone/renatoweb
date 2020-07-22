938080bcd03aca4906238a6e5af782ff37ef31f0
Aline Ebone <iniebone@gmail.com>
2020-07-21 22:55:11 -0300

    Add dark theme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Wisdom from "./Wisdom";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./style/Themes";
+import { GlobalStyles } from "./style/GlobalStyles";
 const axios = require("axios").default;
 const URL =
   "https://raw.githubusercontent.com/alinebone/MinutesOfWisdomAPI/master/minutesOfWisdom.json";
@@ -8,7 +11,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { wisdomQuotes: [] };
+    this.state = { wisdomQuotes: [], theme: "light" };
     this.getWisdom();
   }
 
@@ -19,13 +22,27 @@ class App extends React.Component {
       .catch((error) => console.error(`Wisdom not received: ${error}`));
   };
 
+  changeTheme = () => {
+    this.state.theme === "light"
+      ? this.setState({ theme: "dark" })
+      : this.setState({ theme: "light" });
+  };
+
   render() {
     return (
-      <>
+      <ThemeProvider
+        theme={this.state.theme === "light" ? lightTheme : darkTheme}
+      >
+        <GlobalStyles />
+        <button onClick={this.changeTheme}>
+          {this.state.theme === "light"
+            ? "Switch to Dark Theme"
+            : "Switch to Light Theme"}
+        </button>
         <h1>Oi, Renato</h1>
         <h2>Que estes ensinamentos te tragam paz</h2>
         <Wisdom wisdomQuotes={this.state.wisdomQuotes} />
-      </>
+      </ThemeProvider>
     );
   }
 }
